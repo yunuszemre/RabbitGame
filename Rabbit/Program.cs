a39a9b2ce8edb5006fc7f48e6e8fa763d5ba2128
emre.teke <emre.teke@detaysoft.com>
2024-04-01 07:42:31 +0300

    init

--- Rabbit/Program.cs
+++ Rabbit/Program.cs
@@ -1,7 +1,10 @@
-﻿internal class Program
+﻿using System.Runtime.InteropServices;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        SetFullScreen();
         Console.BackgroundColor = ConsoleColor.Black;
         int xPos = 10;
         int yPos = 8;
@@ -10,14 +13,16 @@
         {
             var x = Console.WindowWidth;
             var y = Console.WindowHeight;
+            Console.WindowWidth = 123;
+            Console.WindowHeight = 63;
             Console.Clear();
             if (x > 120)
             {
                 Console.SetCursorPosition(120, 1);
             }
             Console.WriteLine($"xPos: {xPos}, yPos: {yPos}");
-            DrawRows(15, 75, 100);
-            DrawColumns(25, 125, 60);
+            DrawRows(25, 100, 125);
+            DrawColumns(25, 125, 100);
             DrawRabbit(xPos, yPos);
             var key = Console.ReadKey();
             MoveRabbit(key.Key, ref xPos, ref yPos);
@@ -120,6 +125,21 @@
                 Console.SetCursorPosition(i * columnWidth, j);
                 Console.WriteLine("*");
             }
+        }
+    }
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+    private const int SW_MAXIMIZE = 3;
+    private const int SW_RESTORE = 9;
+    private static void SetFullScreen()
+    {
+        IntPtr hwnd = FindWindow(null, Console.Title);
+        if (hwnd != IntPtr.Zero)
+        {
+            ShowWindow(hwnd, SW_MAXIMIZE);
         }
     }
 }
